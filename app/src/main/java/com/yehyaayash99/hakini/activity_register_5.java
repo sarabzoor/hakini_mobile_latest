8ca7402957a109b08eaa60319dc9a4905fdbcfd4
sara bzoor <sarabzoor1997@gmail.com>
2021-01-26 13:16:25 +0200

    release

---
+++ app/src/main/java/com/yehyaayash99/hakini/activity_register_5.java
@@ -0,0 +1,14 @@
+package com.yehyaayash99.hakini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_5);
+    }
+}
---
+++ app/src/main/java/com/yehyaayash99/hakini/activity_register_6.java
@@ -0,0 +1,14 @@
+package com.yehyaayash99.hakini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_6);
+    }
+}
